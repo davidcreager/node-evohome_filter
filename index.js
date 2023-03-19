ee8da2f3ec6c60da5bc3784f7e59f7441e4e118c
davidcreager <david.h.creager@btinternet.com>
2023-03-19 17:37:57 +0000

    Adding config file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 'use strict';
 const util = require("util");
 const nconf = require("nconf");
-
+const mqtt = require('mqtt')
 
 
 let configFileName = null;
@@ -14,44 +14,39 @@ if (process.argv.length > 2) {
 //console.log("[startServer]\t\t\t argv length=" + process.argv.length + " fname=" +configFileName )
 if (!configFileName.includes(".")) configFileName = configFileName + ".json"
 if (!configFileName.includes("/")) configFileName = "./" + configFileName
-console.log("[startServer]\t\t\t Loading Configuaration from " + configFileName)
+console.log("[evohome_filter]\t Loading Configuaration from " + configFileName)
 const config = nconf.file(configFileName).get().config;
+const subs = config.subscriptions;
+const inp_client  = mqtt.connect(config.inputMQTT);
+const out_client  = mqtt.connect(config.outputMQTT);
+console.log("[evohome_filter]\t input = " + config.inputMQTT + " output = " + config.outputMQTT + " subs =" + subs.join(", "))
 
 
-const mqtt = require('mqtt')
-const inp_client  = mqtt.connect('mqtt://localhost')
-const out_client  = mqtt.connect('mqtt://CREAGERS-WHS')
-
-const subs = [
-	"evohome/evogateway/+/+/relay_demand/#",
-	"evohome/evogateway/+/+/heat_demand/#",
-	"evohome/evogateway/+/+/system_fault/#"
-	]
 
 inp_client.on('error', function (error) {
-	console.log("[evohome_filter] Error received - localhost", error);
+	console.log("[evohome_filter]\t Error received on " + config.inputMQTT, error);
 	if (error.code == "ECONNREFUSED" && error.syscall == "connect") process.exit();
 });
 
 out_client.on('error', function (error) {
-	console.log("[evohome_filter] Error received - creagers", error);
+	console.log("[evohome_filter]\t Error received on " + config.outputMQTT, error);
 	if (error.code == "ECONNREFUSED" && error.syscall == "connect") process.exit();
 });
 
 inp_client.on('connect', function () {
-	console.log("[evohome_filter] Connected to localhost");
+	console.log("[evohome_filter]\t Connected to " + config.inputMQTT);
 	inp_client.subscribe(subs, function (err) {
 		if (err) {
-			console.error("[evohome_filter] Error in subscribing", err)
+			console.error("[evohome_filter]\t Error in subscribing ", err)
 		}
 	})
 });
 out_client.on('connect', function () {
-	console.log("[evohome_filter] Connected to creagers");
+	console.log("[evohome_filter]\t Connected to " + config.outputMQTT);
 })
 
 inp_client.on('message', function (topic, message) {
   // message is Buffer
-  console.log(topic, message.toString())
+  if (config.debug) console.log(topic, message.toString())
   out_client.publish(topic, message, (err) => {if (err) console.log("[evohome_filter] Error in publishing ", err, topic, message)} );
 })
